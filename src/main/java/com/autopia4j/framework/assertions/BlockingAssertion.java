ed2013767532f1cbd2e5964353a30e73570026ba
Vijay Ramaswamy <vijay.ramaswamy@icims.com>
2016-11-28 10:30:25 -0500

    Improved BasicAssertions class
    * Expanded BasicAssertions to include the capability to set the
    stopExecution framework parameters while evaluating an assertion
    * Bumped up POM version

--- src/main/java/com/autopia4j/framework/assertions/BlockingAssertion.java
+++ src/main/java/com/autopia4j/framework/assertions/BlockingAssertion.java
@@ -1,12 +1,14 @@
 package com.autopia4j.framework.assertions;
 
 import com.autopia4j.framework.core.AutopiaException;
+import com.autopia4j.framework.core.FrameworkParameters;
 import com.autopia4j.framework.reporting.Report;
 import com.autopia4j.framework.reporting.Status;
 
 public class BlockingAssertion {
 	
 	private final Report report;
+	private final FrameworkParameters frameworkParameters = FrameworkParameters.getInstance();
 	
 	public BlockingAssertion(Report report) {
 		this.report = report;
@@ -20,11 +22,21 @@ public class BlockingAssertion {
 		}
 	}
 	
+	public void assertTrue(Boolean condition, String message, Boolean stopExecution) {
+		frameworkParameters.setStopExecution(!condition && stopExecution);
+		this.assertTrue(condition, message);
+	}
+	
 	public void assertFalse(Boolean condition, String message) {
 		if(!condition) {
 			report.updateTestLog(message, "Validation returned [false] as expected", Status.PASS, true);
 		} else {
 			throw new AutopiaException(message, "Expected [false] but found [true]");
 		}
+	}
+	
+	public void assertFalse(Boolean condition, String message, Boolean stopExecution) {
+		frameworkParameters.setStopExecution(condition && stopExecution);
+		this.assertFalse(condition, message);
 	}
 }
